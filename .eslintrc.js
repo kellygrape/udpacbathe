35c27b7d98a304300c69b7a118dce6b840e38293
Kelly Anne Pipe <kellygrape@users.noreply.github.com>
2020-08-01 19:42:04 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'extends': ['eslint:recommended', 'google'],
+  'env': {
+    // For more environments, see here: http://eslint.org/docs/user-guide/configuring.html#specifying-environments
+    'browser': true,
+    'es6': true,
+    'jquery': true
+  },
+  'rules': {
+    // Insert custom rules here
+    // For more rules, see here: http://eslint.org/docs/rules/
+    'no-invalid-this': 'off',
+    'no-var': 'warn',
+    'require-jsdoc': 'off'
+  },
+  'parserOptions': {
+    'sourceType': 'module'
+  }
+}
